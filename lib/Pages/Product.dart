94b2293389c1e01c5bfab683b330dc214e96a482
webmostafij <fuadmostfij4@gmail.com>
2022-05-12 11:48:46 +0600

    adad

--- lib/Pages/Product.dart
+++ lib/Pages/Product.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Product extends StatefulWidget {
@@ -8,27 +9,128 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
+  int _selectedPage = 0;
+  final PageController _pageController = PageController();
+
+  void _changePage(int pageNo) {
+    _selectedPage = pageNo;
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        SizedBox(
+      children: [
+        const SizedBox(
           height: 20.0,
         ),
-        Text(
+        const Text(
           "Just For You",
           style: TextStyle(fontSize: 15.0),
         ),
-        SizedBox(
+        const SizedBox(
           height: 20.0,
         ),
+
+        /// problem kothai.
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StatefulBuilder(builder: (_, setStateee) {
+              return TabButton(
+                text: 'All',
+                selectedPage: _selectedPage,
+                pageNumber: 0,
+                onPress: () {
+                  _changePage(0);
+
+                  // setState(() {
+                  //   _changePage(0);
+                  // });
+                },
+              );
+            }),
+            StatefulBuilder(builder: (_, setStateee) {
+              return TabButton(
+                text: 'Mail',
+                selectedPage: _selectedPage,
+                pageNumber: 1,
+                onPress: () {
+                  _changePage(1);
+
+                  // setState(() {
+                  //   _changePage(0);
+                  // });
+                },
+              );
+            }),
+          ],
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          height: 500,
+          child: PageView(
+            children: <Widget>[
+              Center(
+                child: Text("faf"),
+              ),
+              Center(
+                child: Text("fa"),
+              ),
+            ],
+          ),
+        ),
       ],
+    );
+  }
+}
+
+// TabButton StateFull nichen kano.? vaia stateless e niesilam tkhn r ekta error ashsilo okhane silo j stateless a setstate hbe na amn r ki // ok wait
+
+class TabButton extends StatelessWidget {
+  final String text;
+  final int pageNumber;
+  final int selectedPage;
+  final Function onPress;
+  const TabButton(
+      {Key? key,
+      required this.text,
+      required this.pageNumber,
+      required this.selectedPage,
+      required this.onPress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress(),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: selectedPage == pageNumber
+                  ? Colors.deepOrange
+                  : Colors.black),
+        ),
+      ),
     );
   }
 }
